cf13ba4eae074061f7bd8f8a49e600faa1725285
Simar Vashisht <simarv07@gmail.com>
2023-02-25 18:38:30 -0800

    Finished solver screen and solver screen bute

---
+++ ui/js/solver_screen.js
@@ -0,0 +1,9 @@
+        document.getElementById("solve-brute-force").addEventListener("click", function () {
+            window.location.href = "solved_screen.html";
+        });
+        document.getElementById("solve-csp").addEventListener("click", function () {
+            window.location.href = "solved_screen.html";
+        });
+        document.getElementById("clear").addEventListener("click", function () {
+            window.location.href = "main_menu.html";
+        });
---
+++ ui/js/solver_screen_no_brute.js
@@ -0,0 +1,7 @@
+
+        document.getElementById("solve-csp").addEventListener("click", function () {
+            window.location.href = "solved_screen.html";
+        });
+        document.getElementById("clear").addEventListener("click", function () {
+            window.location.href = "main_menu.html";
+        });
