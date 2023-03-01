1dad22dacac37cc22984b47967fda6ca1ed522d9
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-01 12:30:18 -0800

    puzzle loader has differnt sizes and masks puzzle

--- puzzle_loader.py
+++ puzzle_loader.py
@@ -1,11 +1,45 @@
 import json
-
+import random
 class PuzzleLoader:
     def load(self, size):
-
-        # TODO return different 2D arrays subject to the size argument
-
-        # TODO Mask the value in the solved sudoku board
-        with open('assets/solved_sudoku/sudoku9x9/9x9_sample_0.txt', 'r') as file:
+        """
+        Loads a random sudoku puzzle of the correct size
+        """
+        sample = random.randint(0, 9)
+        with open('assets/solved_sudoku/sudoku{0}x{0}/{0}x{0}_sample_{1}.txt'.format(size, sample), 'r') as file:
             contents = file.read()
-            return json.loads(contents)
+            board = json.loads(contents)
+            board = self.mask_puzzle(board)
+            return board
+    
+    def mask_puzzle(self, board):
+        """
+        randomly sets 75% of a board to 0s and returns the board
+        """
+        side = len(board)
+        size = side*side
+        values_to_mask = int(size * 0.75)
+        masked_values = 0
+        col = 0
+        row = 0
+        while (masked_values <= values_to_mask): 
+            if random.random() < 0.5:
+                board[row][col] = 0
+                masked_values += 1
+            col += 1
+            if col >= side:
+                col = 0
+                row += 1
+                if row >= side:
+                    col = 0
+                    row = 0
+        return board
+                
+def main():
+    loader = PuzzleLoader()
+    print(loader.load(12))
+
+if __name__ == '__main__':
+    main()
+
+        
