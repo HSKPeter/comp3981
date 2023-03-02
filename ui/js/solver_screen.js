ac57552f7b48257f1d3c270f509d8566f0ed3046
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-01 18:52:21 -0800

    fixed 12x12 coloring

--- ui/js/solver_screen.js
+++ ui/js/solver_screen.js
@@ -66,16 +66,22 @@ function fillBoard(board_values) {
         for (let j = 0; j < size; j++) {
             var cell_to_insert = document.createElement('div');
             cell_to_insert.setAttribute('id', 'cell ' + i + "-" + j)
-
-            var set_row = parseInt(i / (Math.sqrt(size)))
+    
+            var set_row = parseInt(i/(Math.sqrt(size)))
             var set_column = parseInt(j / Math.sqrt(size))
-
+            
+            if (size == 12) {
+                set_row = parseInt(j/4)
+                set_column = parseInt(i/3)
+            }
+            
+    
             if (board_values[i][j] == 0)
-                cell_to_insert.innerHTML = '';
-            else
+                cell_to_insert.innerHTML = '';  
+            else 
                 cell_to_insert.innerHTML = board_values[i][j]
-
-
+            
+            
             if ((set_row + set_column) % 2 == 1) {
                 cell_to_insert.style.backgroundColor = "#D3D3D3"
             }
