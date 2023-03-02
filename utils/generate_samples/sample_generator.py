5af3f7270762f4c00584818f029b6ee8b52fc9e6
Peter Hui <huishikuen@gmail.com>
2023-03-01 21:10:11 -0800

    Add samples generator

---
+++ utils/generate_samples/sample_generator.py
@@ -0,0 +1,27 @@
+import sys
+import json
+import abc
+
+class SampleGenerator(abc.ABC):
+    def map_digits_to_int_list(self, digits_str):
+        return [int(digit_char) for digit_char in digits_str]
+
+    def jsonfiy(self, src_filename):
+        with open(f'{self.src_folder}/{src_filename}', 'r') as file:
+            contents = file.read()
+            rows = contents.split("\n")
+            board = [self.map_digits_to_int_list(row) for row in rows]
+            return json.dumps(board)   
+
+    def generate(self, filename):
+        jsonified_content = self.jsonfiy(filename)
+        with open(f'{self.destination_folder}/{filename}', 'w') as file:
+            file.write(jsonified_content)
+
+    def generate_samples(self, filenames):
+        confirmed = input("Are you running this .py program in the directory of \"utils/generate_samples\"? (Y/N) ")
+        if confirmed.strip().upper() == "Y":
+            for filename in filenames:
+                self.generate(filename)
+        else:
+            print("You need to be in the directory of \"utils/generate_samples\" to run this .py program.")
