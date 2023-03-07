9dc27d746ec3d03afc4717f3be2b3cc71b89683e
Peter Hui <huishikuen@gmail.com>
2023-03-06 21:09:36 -0800

    Make solution checking more robust

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,13 +1,17 @@
 import copy
 import math
 
-RAW_BOARD = [
-    [0, 0, 1, 2],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 1, 0, 0]
-]
+NINE_X_NINE = [[0,0,3,0,2,0,6,0,0],[9,0,0,3,0,5,0,0,1],[0,0,1,8,0,6,4,0,0],[0,0,8,1,0,2,9,0,0],[7,0,0,0,0,0,0,0,8],[0,0,6,7,0,8,2,0,0],[0,0,2,6,0,9,5,0,0],[8,0,0,2,0,3,0,0,9],[0,0,5,0,1,0,3,0,0]]
+TWELVE_X_TWELVE = [[0,0,0,11,8,0,0,0,0,0,0,6],[10,0,0,0,0,0,0,0,0,4,0,11],[0,7,0,0,0,0,2,6,0,0,1,0],[7,0,12,0,0,0,9,0,0,0,0,0],[0,0,0,10,6,2,0,0,5,0,0,0],[0,9,0,8,0,10,0,0,0,0,0,0],[0,1,0,0,5,0,7,8,0,0,6,3],[0,0,0,7,2,0,10,0,8,0,12,5],[0,6,0,5,0,0,12,0,0,1,0,0],[6,5,7,12,0,3,8,2,0,11,0,0],[0,0,8,0,0,1,5,0,0,7,3,9],[9,3,4,1,0,0,6,10,12,0,0,0]]
+SIXTEEN_X_SIXTEEN = [[0,3,0,0,0,0,0,2,0,0,10,14,13,0,0,0],[0,0,0,0,0,0,0,0,16,15,0,1,0,0,0,6],[0,11,0,10,9,1,0,0,0,0,0,2,0,14,0,0],[0,0,0,8,0,16,0,14,0,0,0,0,0,0,9,0],[0,0,0,0,15,0,13,7,0,0,12,0,0,0,0,10],[0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,14,0,0,0,4,0,13,0,0,0,12],[0,13,0,0,0,0,0,0,0,0,5,0,0,0,0,0],[0,0,0,6,0,0,9,0,14,0,0,0,0,13,1,0],[15,0,0,0,5,0,3,6,0,0,0,0,0,2,0,0],[13,16,2,0,0,0,0,0,3,12,6,0,0,0,0,9],[0,0,0,0,0,13,0,4,9,0,1,7,0,8,6,0],[16,15,0,0,0,0,0,0,8,1,0,10,0,0,7,2],[0,0,0,11,0,0,0,0,2,14,0,0,0,0,0,4],[5,10,0,2,16,12,7,0,15,11,4,6,0,9,0,14],[1,0,0,4,2,8,0,9,0,5,16,3,11,12,10,0]]
+TWENTY_FIVE_X_TWENTY_FIVE = [[0,0,0,0,0,20,0,0,9,0,25,14,0,0,0,0,0,7,0,0,0,0,0,0,0],[21,0,6,18,20,1,0,0,0,0,0,11,4,0,0,0,0,0,0,0,0,14,0,0,22],[0,0,0,3,22,0,8,0,0,7,23,0,0,2,0,0,0,0,0,0,0,0,0,0,0],[0,10,0,0,0,0,3,6,0,0,0,0,0,0,0,0,0,0,18,0,0,24,11,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,21,0,0,9,10,0,0,0],[0,0,0,0,0,0,24,0,0,14,0,0,12,0,17,0,1,0,6,25,0,0,0,21,0],[0,0,14,0,24,12,0,0,0,0,0,0,0,0,6,0,0,2,0,22,10,0,7,3,0],[0,0,0,0,0,22,0,0,0,0,14,21,0,25,0,0,0,0,0,0,0,0,0,0,0],[6,21,18,0,0,0,0,0,0,0,11,0,10,0,0,0,16,0,5,0,0,0,12,0,0],[0,0,0,0,10,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,5,2,0,0],[0,0,0,0,0,0,2,17,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,25,0,0,0,4,0,16,3,0,9,0,22,0,18,0,17,15,14,0,11,0,24,0],[8,0,0,0,14,0,25,19,23,0,0,0,15,0,0,0,21,0,0,0,4,3,9,10,7],[0,4,0,0,0,24,0,7,5,0,0,17,0,0,2,1,6,0,0,20,0,21,19,16,13],[0,13,0,0,2,0,0,0,0,0,0,0,25,0,0,0,7,0,0,4,1,0,0,18,6],[11,0,22,9,4,0,5,0,0,0,0,12,0,17,23,25,0,0,0,6,13,19,16,15,0],[24,0,0,17,0,0,0,0,0,18,0,1,0,0,0,22,2,0,13,21,7,9,10,0,0],[0,2,15,5,0,11,0,0,22,0,21,16,0,3,0,0,4,12,0,0,0,0,18,0,0],[0,18,20,0,0,17,1,2,0,0,0,0,11,0,0,0,9,16,3,0,0,25,0,0,8],[10,12,7,25,0,0,16,0,3,6,0,13,0,9,0,15,0,0,17,0,0,0,24,5,0],[3,0,11,4,9,0,0,8,0,5,0,0,0,12,0,6,0,25,20,0,0,0,15,19,0],[14,8,24,16,17,0,19,12,0,23,6,0,0,0,5,21,0,22,0,11,0,7,0,0,10],[2,0,19,13,5,0,0,0,0,20,0,0,21,16,0,7,0,0,0,12,0,6,0,0,0],[0,20,23,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,9,16,0,12,22,25,14],[12,0,0,0,0,0,21,3,0,0,9,0,0,13,22,0,17,15,14,18,0,0,0,0,0]]
+RAW_BOARD = NINE_X_NINE
 
+SQUARE_ROOTS = {
+    9: 3,
+    25: 5,
+    100: 10
+}
 
 class SudokuSolver:
     def __init__(self, raw_board: [[int]]) -> None:
@@ -16,6 +20,7 @@ class SudokuSolver:
 
     def solve(self):
         while len(self.stack) > 0:
+            # print(f"Stack size: {len(self.stack)}")
             current_node = self.stack[-1]
             if current_node.is_solution():
                 return current_node.board
@@ -30,6 +35,7 @@ class SudokuSolver:
 class Node:
     def __init__(self, board: [[int]]) -> None:
         self.board = board
+        self.board_length = len(self.board)
         self.children = []
         self.find_valid_children()
 
@@ -39,10 +45,31 @@ class Node:
         self.isChecked = True
 
     def is_solution(self):
-        for row in self.board:
-            for cell in row:
-                if cell == 0:
+        """
+        To become a solution, it is required that:
+        (1) All cells are filled
+        (2) No duplicated value in each row
+        (3) No duplicated value in each column
+        (4) No duplicated value in each sub square
+        """
+        sqr_root = SQUARE_ROOTS[self.board_length]
+        col_values_sets = [set() for i in range(self.board_length)]
+        sub_square_values_sets = [set() for i in range(self.board_length)]
+        for row_index in range(self.board_length):
+            row_values_set = set()
+            for col_index in range(self.board_length):
+                sub_square_index = math.floor(row_index / sqr_root) * sqr_root + math.floor(col_index / sqr_root)
+                sub_square_values_set = sub_square_values_sets[sub_square_index]
+                cell = self.board[row_index][col_index]
+                col_values_set = col_values_sets[col_index]
+                if (cell == 0) or \
+                   (cell in row_values_set) or \
+                   (cell in col_values_set) or \
+                   (cell in sub_square_values_set):
                     return False
+                row_values_set.add(cell)   
+                col_values_set.add(cell)   
+                sub_square_values_set.add(cell)          
         return True
 
     def get_first_unchecked_child(self):
@@ -51,6 +78,7 @@ class Node:
                 return node
         return None
 
+    # Maybe we could make good use of `yield` to enhance the performance for large board e.g. 25x25
     def find_valid_children(self):
         n = len(self.board)
         for row in range(n):
