efaf33c8d3754b26eaf902c458990874eec32c0d
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-06 16:23:43 -0800

    sudoku solver working prototype

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,9 +1,9 @@
-from types import Union
 import copy
+import math
 
 RAW_BOARD = [
     [0, 0, 1, 2],
-    [2, 0, 0, 0],
+    [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 1, 0, 0]
 ]
@@ -11,65 +11,75 @@ RAW_BOARD = [
 
 class SudokuSolver:
     def __init__(self, raw_board: [[int]]) -> None:
-        self.board = self.initialize_board(raw_board)
+        self.board = raw_board
         self.stack = [Node(self.board)]
 
-    def initialize_board(self, raw_board: [[int]]):
-        return [[Cell(value) for value in row] for row in raw_board]
-
     def solve(self):
-        while len(node_stack) > 0:
-            node = self.stack[-1]
-            node.find_children()
-
-
-# class Board:
-#     def __init__(self, values: [[Cell]]):
-#         self.values = values
-#
-#     def updateCell(self, cell, value):
-
-
-class Cell:
-    def __init__(self, value: int):
-        self.value = value
-        self.domain = list(range(1, 5))
+        while len(self.stack) > 0:
+            current_node = self.stack[-1]
+            if current_node.is_solution():
+                return current_node.board
+            next_node = current_node.get_first_unchecked_child()
+            if next_node is None:
+                self.stack.pop()
+                current_node.check()
+            else:
+                self.stack.append(next_node)
 
 
 class Node:
-    def __init__(self, board: [[Cell]]) -> None:
-        self.boagird = board
-        # self.parent = parent
+    def __init__(self, board: [[int]]) -> None:
+        self.board = board
         self.children = []
+        self.find_valid_children()
+
         self.isChecked = False
 
     def check(self):
         self.isChecked = True
 
+    def is_solution(self):
+        for row in self.board:
+            for cell in row:
+                if cell == 0:
+                    return False
+        return True
+
+    def get_first_unchecked_child(self):
+        for node in self.children:
+            if not node.isChecked:
+                return node
+        return None
+
     def find_valid_children(self):
-        next_cell = None
-        for row in range(len(self.board)):
-            for col in range(row):
+        n = len(self.board)
+        for row in range(n):
+            for col in range(n):
                 if self.board[row][col] == 0:
-                    new_board = copy.deepcopy(self.board)
-                    children = []
-                    for i in range(1, 5):
-                        new_board[row][col] = i
-                        children.append(Node(new_board))
-                        self.children = children
-                        return self.children
+                    for i in range(1, n + 1):
+                        if self.is_valid_insertion(row, col, i):
+                            new_board = copy.deepcopy(self.board)
+                            new_board[row][col] = i
+                            self.children.append(Node(new_board))
+                    return
 
     def is_valid_insertion(self, row, col, new_value):
-        squares = self.getSubsquares()
-        rows = self.getRows()
-        columns = self.getColumns()
+        rows = self.get_rows()
         if new_value in rows[row]:
             return False
+
+        columns = self.get_columns()
         if new_value in columns[col]:
             return False
-        
 
-    def getSubsquares(self):
+        sub_index = self.get_sub_square_index(row, col)
+        squares = self.get_subsquares()
+        if new_value in squares[sub_index]:
+            return False
+
+        return True
+
+    def get_subsquares(self):
         board = self.board
         n = len(board)
         subsquares = []
@@ -83,7 +93,14 @@ class Node:
                 subsquares.append(subsquare)
         return subsquares
 
-    def getRows(self):
+    def get_sub_square_index(self, row, col):
+        subsize = int(math.sqrt(len(self.board)))
+        subRow = row // subsize
+        subCol = col // subsize
+        subIndex = subRow * subsize + subCol
+        return subIndex
+
+    def get_rows(self):
         board = self.board
         rows = []
         for i in range(len(board)):
@@ -91,7 +108,7 @@ class Node:
             rows.append(row)
         return rows
 
-    def getColumns(self):
+    def get_columns(self):
         board = self.board
         columns = []
         for j in range(len(board)):
@@ -100,3 +117,13 @@ class Node:
                 column.add(board[i][j])
             columns.append(column)
         return columns
+
+
+def main():
+    solver = SudokuSolver(RAW_BOARD)
+    solution = solver.solve()
+    print("solution: ", solution)
+
+
+if __name__ == '__main__':
+    main()
