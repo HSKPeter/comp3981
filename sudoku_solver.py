a9f01ddb15072e33c084815a5fb80a921f253fa9
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-07 18:05:12 -0800

    working code with heuristic for  cell to expand

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,10 +1,11 @@
 import copy
 import math
 from typing import List
+import random
 
 NINE_X_NINE = [[0,0,3,0,2,0,6,0,0],[9,0,0,3,0,5,0,0,1],[0,0,1,8,0,6,4,0,0],[0,0,8,1,0,2,9,0,0],[7,0,0,0,0,0,0,0,8],[0,0,6,7,0,8,2,0,0],[0,0,2,6,0,9,5,0,0],[8,0,0,2,0,3,0,0,9],[0,0,5,0,1,0,3,0,0]]
 TWELVE_X_TWELVE = [[0,0,0,11,8,0,0,0,0,0,0,6],[10,0,0,0,0,0,0,0,0,4,0,11],[0,7,0,0,0,0,2,6,0,0,1,0],[7,0,12,0,0,0,9,0,0,0,0,0],[0,0,0,10,6,2,0,0,5,0,0,0],[0,9,0,8,0,10,0,0,0,0,0,0],[0,1,0,0,5,0,7,8,0,0,6,3],[0,0,0,7,2,0,10,0,8,0,12,5],[0,6,0,5,0,0,12,0,0,1,0,0],[6,5,7,12,0,3,8,2,0,11,0,0],[0,0,8,0,0,1,5,0,0,7,3,9],[9,3,4,1,0,0,6,10,12,0,0,0]]
-SIXTEEN_X_SIXTEEN = [[0,3,0,0,0,0,0,2,0,0,10,14,13,0,0,0],[0,0,0,0,0,0,0,0,16,15,0,1,0,0,0,6],[0,11,0,10,9,1,0,0,0,0,0,2,0,14,0,0],[0,0,0,8,0,16,0,14,0,0,0,0,0,0,9,0],[0,0,0,0,15,0,13,7,0,0,12,0,0,0,0,10],[0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,14,0,0,0,4,0,13,0,0,0,12],[0,13,0,0,0,0,0,0,0,0,5,0,0,0,0,0],[0,0,0,6,0,0,9,0,14,0,0,0,0,13,1,0],[15,0,0,0,5,0,3,6,0,0,0,0,0,2,0,0],[13,16,2,0,0,0,0,0,3,12,6,0,0,0,0,9],[0,0,0,0,0,13,0,4,9,0,1,7,0,8,6,0],[16,15,0,0,0,0,0,0,8,1,0,10,0,0,7,2],[0,0,0,11,0,0,0,0,2,14,0,0,0,0,0,4],[5,10,0,2,16,12,7,0,15,11,4,6,0,9,0,14],[1,0,0,4,2,8,0,9,0,5,16,3,11,12,10,0]]
+SIXTEEN_X_SIXTEEN = [[15, 8, 16, 14, 9, 13, 6, 1, 5, 3, 7, 4, 10, 2, 11, 12], [6, 12, 11, 7, 5, 3, 10, 8, 1, 14, 9, 2, 13, 4, 16, 15], [5, 13, 3, 10, 7, 16, 2, 4, 8, 11, 15, 12, 9, 6, 1, 14], [9, 1, 2, 4, 11, 15, 14, 12, 16, 10, 13, 6, 8, 5, 7, 3], [8, 7, 14, 13, 16, 5, 11, 10, 12, 15, 4, 1, 2, 3, 6, 9], [16, 3, 10, 11, 13, 14, 4, 15, 7, 2, 6, 9, 5, 1, 12, 8], [1, 9, 5, 15, 8, 6, 12, 2, 10, 13, 3, 11, 14, 16, 4, 7], [2, 6, 4, 12, 3, 7, 1, 9, 14, 16, 5, 8, 15, 11, 13, 10], [10, 4, 8, 16, 14, 12, 5, 7, 9, 1, 11, 13, 6, 15, 3, 2], [11, 2, 9, 5, 15, 4, 16, 13, 6, 12, 8, 3, 7, 14, 10, 1], [12, 15, 7, 6, 1, 10, 8, 3, 2, 4, 14, 5, 16, 13, 9, 11], [13, 14, 1, 3, 2, 11, 9, 6, 15, 7, 16, 10, 12, 8, 5, 4], [14, 5, 12, 1, 6, 8, 13, 11, 3, 9, 2, 7, 4, 10, 15, 16], [3, 10, 6, 9, 4, 1, 15, 14, 13, 8, 12, 16, 11, 7, 2, 5], [7, 11, 13, 8, 12, 2, 3, 16, 4, 5, 10, 15, 1, 9, 14, 6], [4, 16, 15, 2, 10, 9, 7, 5, 11, 6, 1, 14, 3, 12, 8, 13]]
 TWENTY_FIVE_X_TWENTY_FIVE = [[0,0,0,0,0,20,0,0,9,0,25,14,0,0,0,0,0,7,0,0,0,0,0,0,0],[21,0,6,18,20,1,0,0,0,0,0,11,4,0,0,0,0,0,0,0,0,14,0,0,22],[0,0,0,3,22,0,8,0,0,7,23,0,0,2,0,0,0,0,0,0,0,0,0,0,0],[0,10,0,0,0,0,3,6,0,0,0,0,0,0,0,0,0,0,18,0,0,24,11,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,21,0,0,9,10,0,0,0],[0,0,0,0,0,0,24,0,0,14,0,0,12,0,17,0,1,0,6,25,0,0,0,21,0],[0,0,14,0,24,12,0,0,0,0,0,0,0,0,6,0,0,2,0,22,10,0,7,3,0],[0,0,0,0,0,22,0,0,0,0,14,21,0,25,0,0,0,0,0,0,0,0,0,0,0],[6,21,18,0,0,0,0,0,0,0,11,0,10,0,0,0,16,0,5,0,0,0,12,0,0],[0,0,0,0,10,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,5,2,0,0],[0,0,0,0,0,0,2,17,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,25,0,0,0,4,0,16,3,0,9,0,22,0,18,0,17,15,14,0,11,0,24,0],[8,0,0,0,14,0,25,19,23,0,0,0,15,0,0,0,21,0,0,0,4,3,9,10,7],[0,4,0,0,0,24,0,7,5,0,0,17,0,0,2,1,6,0,0,20,0,21,19,16,13],[0,13,0,0,2,0,0,0,0,0,0,0,25,0,0,0,7,0,0,4,1,0,0,18,6],[11,0,22,9,4,0,5,0,0,0,0,12,0,17,23,25,0,0,0,6,13,19,16,15,0],[24,0,0,17,0,0,0,0,0,18,0,1,0,0,0,22,2,0,13,21,7,9,10,0,0],[0,2,15,5,0,11,0,0,22,0,21,16,0,3,0,0,4,12,0,0,0,0,18,0,0],[0,18,20,0,0,17,1,2,0,0,0,0,11,0,0,0,9,16,3,0,0,25,0,0,8],[10,12,7,25,0,0,16,0,3,6,0,13,0,9,0,15,0,0,17,0,0,0,24,5,0],[3,0,11,4,9,0,0,8,0,5,0,0,0,12,0,6,0,25,20,0,0,0,15,19,0],[14,8,24,16,17,0,19,12,0,23,6,0,0,0,5,21,0,22,0,11,0,7,0,0,10],[2,0,19,13,5,0,0,0,0,20,0,0,21,16,0,7,0,0,0,12,0,6,0,0,0],[0,20,23,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,9,16,0,12,22,25,14],[12,0,0,0,0,0,21,3,0,0,9,0,0,13,22,0,17,15,14,18,0,0,0,0,0]]
 
 
@@ -25,20 +26,22 @@ class SudokuSolver:
         while stack_size > 0:
             # print(f"Stack size: {len(self.stack)}")
             current_node = self.stack[-1]
-            
             if current_node.is_solution():
                 print(f"result found: i = {i}")
                 return current_node
+            current_node.visit()
             next_node = current_node.get_first_unchecked_child()
             if next_node is None:
-                self.stack.pop()
                 current_node.check()
+                self.stack.remove(current_node)
             else:
                 self.stack.append(next_node)
             i += 1
             stack_size = len(self.stack)
-            if i % 100 == 0:
+            if i % 1000 == 0:
+                print()
                 print(f"searching (i = {i}; stack size = {stack_size})")
+                # print("stack", self.stack)
                 print(current_node)
 
 
@@ -47,9 +50,14 @@ class Node:
         self.board = board
         self.board_length = len(self.board)
         self.children = []
-        self.find_valid_children()
-
+        self.domains = self.find_domains()
         self.isChecked = False
+        self.isVisited = False
+
+    def visit(self):
+        if not self.isVisited:
+            self.find_valid_children()
+            self.isVisited = True
 
     def check(self):
         self.isChecked = True
@@ -78,13 +86,11 @@ class Node:
 
     # Maybe we could make good use of `yield` to enhance the performance for large board e.g. 25x25
     def find_valid_children(self):
-        n = len(self.board)
         cell = self.find_min_domain_cell()
         if cell is None:
             return
         row, col = cell
-        for i in range(1, n + 1):
-            print("checking", i)
+        for i in self.domains[row][col]:
             if self.is_valid_insertion(row, col, i):
                 new_board = copy.deepcopy(self.board)
                 new_board[row][col] = i
@@ -93,7 +99,7 @@ class Node:
     def find_min_domain_cell(self):
         board = self.board
         n = len(board)
-        domains = self.find_domains()
+        domains = self.domains
         min_domain_size = n+1
         min_domain_cell = None
         for i in range(n):
@@ -144,48 +150,68 @@ class Node:
         # if none of the checks failed, the insertion is valid
         return True
 
-    def get_subsquares(self):
-        board = self.board
-        n = len(board)
-        subsquares = []
-        subsize = int(n ** 0.5)
-        for i in range(0, n, subsize):
-            for j in range(0, n, subsize):
-                subsquare = set()
-                for x in range(i, i + subsize):
-                    for y in range(j, j + subsize):
-                        subsquare.add(board[x][y])
-                subsquares.append(subsquare)
-        return subsquares
+    # def get_subsquares(self):
+    #     board = self.board
+    #     n = len(board)
+    #     subsquares = []
+    #     subsize = int(n ** 0.5)
+    #     for i in range(0, n, subsize):
+    #         for j in range(0, n, subsize):
+    #             subsquare = set()
+    #             for x in range(i, i + subsize):
+    #                 for y in range(j, j + subsize):
+    #                     subsquare.add(board[x][y])
+    #             subsquares.append(subsquare)
+    #     return subsquares
+    #
+    # def get_sub_square_index(self, row, col):
+    #     subsize = int(math.sqrt(len(self.board)))
+    #     subRow = row // subsize
+    #     subCol = col // subsize
+    #     subIndex = subRow * subsize + subCol
+    #     return subIndex
+    #
+    # def get_rows(self):
+    #     board = self.board
+    #     rows = []
+    #     for i in range(len(board)):
+    #         row = set(board[i])
+    #         rows.append(row)
+    #     return rows
+    #
+    # def get_columns(self):
+    #     board = self.board
+    #     columns = []
+    #     for j in range(len(board)):
+    #         column = set()
+    #         for i in range(len(board)):
+    #             column.add(board[i][j])
+    #         columns.append(column)
+    #     return columns
 
-    def get_sub_square_index(self, row, col):
-        subsize = int(math.sqrt(len(self.board)))
-        subRow = row // subsize
-        subCol = col // subsize
-        subIndex = subRow * subsize + subCol
-        return subIndex
 
-    def get_rows(self):
-        board = self.board
-        rows = []
-        for i in range(len(board)):
-            row = set(board[i])
-            rows.append(row)
-        return rows
+def change_values_to_zero(arr, p=0.75):
+    """
+    Takes an n x n array and changes p percent of the values to 0.
 
-    def get_columns(self):
-        board = self.board
-        columns = []
-        for j in range(len(board)):
-            column = set()
-            for i in range(len(board)):
-                column.add(board[i][j])
-            columns.append(column)
-        return columns
+    Args:
+        arr: list[list[int]] - an n x n array
+        p: float - the proportion of values to change to 0 (default 0.75)
 
+    Returns:
+        list[list[int]] - the modified n x n array
+    """
+    n = len(arr)
+    m = int(p * n * n)  # number of values to change to 0
+    indices = random.sample(range(n * n), m)  # choose random indices to change
+    for idx in indices:
+        i, j = divmod(idx, n)  # calculate the row and column indices
+        arr[i][j] = 0  # set the value to 0
+    return arr
 
 def main():
-    solver = SudokuSolver(SIXTEEN_X_SIXTEEN)
+    board = change_values_to_zero(SIXTEEN_X_SIXTEEN)
+    solver = SudokuSolver(board)
     solution_node = solver.solve()
     print(f"solution:\n{solution_node}")
     board = solution_node.board
