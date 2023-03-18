b179c929f3e8e3295c3009b9c7f1efaeab101991
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-18 12:08:44 -0700

    added a base CSP class

--- sudoku_solver_csp.py
+++ sudoku_solver_csp.py
@@ -35,3 +35,49 @@ def is_consistent(value, var, assignment, csp):
 
 def inference(csp, var, assignment):
     pass
+
+ROW = 0
+COL = 0
+class SudokuCSP:
+    
+    def __init__(self, initial_grid=None) -> None:
+        self.variables = [(row, col) for row in range(9) for col in range (9)]
+        self.domains = {(row, col): set(range(1, 10)) for row in range(9) for col in range(9)}
+        self.constraints = self.build_constraints()
+
+        # Set the initial values in the grid
+        for row in range(9):
+            for col in range(9):
+                value = initial_grid[row][col]
+                if value != 0:
+                    self.assign_value(row, col, value)
+
+    def build_constraints(self):
+        def row_constraint(cell1, cell2):
+            return cell1[ROW] == cell2[ROW]
+
+        def col_constraint(cell1, cell2):
+            return cell1[COL] == cell2[COL]
+
+        def box_constraint(cell1, cell2):
+            return (cell1[ROW] // 3 == cell2[ROW] // 3) and (cell1[COL] // 3 != cell2[COL] // 3)
+
+        constraints = set()
+        for cell1 in self.variables:
+            for cell2 in self.variables:
+                if cell1 != cell2:
+                    if row_constraint(cell1, cell2) or col_constraint(cell1, cell2) or box_constraint(cell1, cell2):
+                        constraints.add((cell1, cell2))
+        return constraints
+    
+    def assign_value(self, row, col, value):
+        if value in self.domains[(row, col)]:
+            self.domains[(row, col)] = {value}
+
+    def add_inferences(self, inferences):
+        pass  # Implement adding inferences
+
+    def remove_inferences(self, inferences):
+        pass  # Implement removing inferences
+
+csp = SudokuCSP()
