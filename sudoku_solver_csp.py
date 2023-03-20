87388ab3ba9c687eb39ba4b5c56b1c151df2f050
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-20 12:02:06 -0700

    added comments explaining how to implement methods

--- sudoku_solver_csp.py
+++ sudoku_solver_csp.py
@@ -1,4 +1,3 @@
-
 assignemnt = {(3, 5): 4}
 
 
@@ -19,9 +18,37 @@ class Assignments:
         pass
 
     def select_unassigned_variable(self, constraints):
+        """ 
+        Selecting unassigned variables: Use a combination of the Minimum Remaining Values (MRV) and Degree heuristics.
+        MRV: Choose the variable with the fewest legal values remaining in its domain.
+        Degree: Choose the variable involved in the highest number of constraints with other unassigned variables.
+
+        Minimum Remaining Values (MRV):
+T       he MRV heuristic chooses the unassigned variable with the fewest legal values remaining in its domain. In other words, it selects the variable that has the smallest number of possible values left to be assigned. The idea behind MRV is to select the variable that is most likely to cause a failure early on, thus pruning the search tree and reducing the overall search time.
+
+        Degree:
+        The Degree heuristic chooses the unassigned variable that is involved in the highest number of constraints with other unassigned variables. By selecting a variable with a high degree, you can potentially affect the most other unassigned variables in the problem. The Degree heuristic helps to resolve potential conflicts earlier in the search, which can also reduce the overall search time.
+
+        While both MRV and Degree heuristics aim to improve the efficiency of the search process, they focus on different aspects of the problem. MRV looks at the remaining possibilities for a variable, while Degree looks at the relationships and constraints between unassigned variables. In some cases, it's beneficial to use a combination of both heuristics to select the most promising unassigned variable for the next step in the search process.
+        """
         pass
 
     def find_order_domain_values_of_var(self, var, constraints):
+        """
+        Ordering values of a variable: Use the Least Constraining Value (LCV) heuristic, 
+        which selects the value that imposes the fewest constraints on the remaining variables.
+        The Least Constraining Value (LCV) heuristic is used to order the values of a variable when attempting to assign a value during the search process in a Constraint Satisfaction Problem (CSP). The LCV heuristic aims to minimize the impact of the current assignment on the future assignments of other variables.
+
+        The main idea behind the LCV heuristic is to choose a value that imposes the fewest constraints on the remaining unassigned variables. By doing so, you keep more options open for the remaining variables, which can potentially result in fewer backtracks and a more efficient search process.
+
+        To implement the LCV heuristic, you need to perform the following steps:
+
+        1. For the current variable you are assigning a value to, evaluate each possible value in its domain.
+        2. For each value, count the number of constraints it imposes on the remaining unassigned variables. This typically involves counting how many legal values are eliminated from the domains of neighboring 3. unassigned variables if the current value is assigned to the current variable.
+        4. Order the values by the number of constraints they impose, from least constraining to most constraining.
+        3. Assign the values to the current variable in the order determined by the LCV heuristic.
+        By using the LCV heuristic, you can increase the likelihood of finding a solution without the need for excessive backtracking. This can result in a more efficient search process and, ultimately, a faster solution to the CSP. 
+        """
         pass
 
 
@@ -44,7 +71,8 @@ class Constraints:
 def new_backtrack(constraints, assignment):
     if assignment.is_complete():
         return assignment
-    var = assignment.select_unassigned_variable(constraints)  # var = (row, col, sub_square)
+    var = assignment.select_unassigned_variable(
+        constraints)  # var = (row, col, sub_square)
     for value in assignment.find_order_domain_values_of_var(var, constraints):
         if assignment.is_consistent(value, var, constraints):
             assignment.add(var, value)
@@ -59,10 +87,11 @@ def new_backtrack(constraints, assignment):
             del assignemnt[var]
     return "failure"
 
+
 def backtrack(csp, assignment):
     if is_complete(assignment, csp):
         return assignment
-    var = select_unassigned_variable(csp, assignment) # var = (row, col)
+    var = select_unassigned_variable(csp, assignment)  # var = (row, col)
     for value in order_domain_values(csp, var, assignment):
         if is_consistent(value, var, assignment, csp):
             assignment[var] = value
@@ -84,22 +113,29 @@ def is_complete(assignment, csp):
 def select_unassigned_variable(csp, assignment):
     pass
 
+
 def order_domain_values(csp, var, assignment):
-    pass 
+    pass
+
 
 def is_consistent(value, var, assignment, csp):
     pass
 
+
 def inference(csp, var, assignment):
     pass
 
+
 ROW = 0
 COL = 0
+
+
 class SudokuCSP:
-    
+
     def __init__(self, initial_grid=None) -> None:
-        self.variables = [(row, col) for row in range(9) for col in range (9)]
-        self.domains = {(row, col): set(range(1, 10)) for row in range(9) for col in range(9)}
+        self.variables = [(row, col) for row in range(9) for col in range(9)]
+        self.domains = {(row, col): set(range(1, 10))
+                        for row in range(9) for col in range(9)}
         self.constraints = self.build_constraints()
 
         # Set the initial values in the grid
@@ -126,7 +162,7 @@ class SudokuCSP:
                     if row_constraint(cell1, cell2) or col_constraint(cell1, cell2) or box_constraint(cell1, cell2):
                         constraints.add((cell1, cell2))
         return constraints
-    
+
     def assign_value(self, row, col, value):
         if value in self.domains[(row, col)]:
             self.domains[(row, col)] = {value}
@@ -136,5 +172,6 @@ class SudokuCSP:
 
     def remove_inferences(self, inferences):
         pass  # Implement removing inferences
+
 
 csp = SudokuCSP()
