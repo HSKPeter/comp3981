817c9ccda3b7137292efd1aba92c3a02092499d6
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-03-23 16:21:42 -0700

    add stuff to time_testing

--- time_testing.py
+++ time_testing.py
@@ -4,14 +4,24 @@ from sudoku_solver_csp import Assignments, Constraints, backtrack
 from sudoku_solver import mask_board, SudokuSolver
 
 NINE_X_NINE_SOLVED = [[7, 6, 2, 4, 9, 8, 5, 1, 3],
-                          [9, 3, 1, 2, 5, 6, 4, 8, 7],
-                          [4, 5, 8, 1, 3, 7, 2, 6, 9],
-                          [5, 1, 9, 7, 8, 2, 3, 4, 6],
-                          [6, 8, 7, 9, 4, 3, 1, 5, 2],
-                          [3, 2, 4, 5, 6, 1, 9, 7, 8],
-                          [2, 9, 6, 8, 1, 4, 7, 3, 5],
-                          [8, 4, 5, 3, 7, 9, 6, 2, 1],
-                          [1, 7, 3, 6, 2, 5, 8, 9, 4]]
+                      [9, 3, 1, 2, 5, 6, 4, 8, 7],
+                      [4, 5, 8, 1, 3, 7, 2, 6, 9],
+                      [5, 1, 9, 7, 8, 2, 3, 4, 6],
+                      [6, 8, 7, 9, 4, 3, 1, 5, 2],
+                      [3, 2, 4, 5, 6, 1, 9, 7, 8],
+                      [2, 9, 6, 8, 1, 4, 7, 3, 5],
+                      [8, 4, 5, 3, 7, 9, 6, 2, 1],
+                      [1, 7, 3, 6, 2, 5, 8, 9, 4]]
+
+
+def benchmark_algos():
+    n = 50  # Number of times to run each algorithm
+    performance_metrics = test_algorithms(NINE_X_NINE_SOLVED, n)
+    for algo_name, metrics in performance_metrics.items():
+        print(f"{algo_name}:")
+        for metric_name, value in metrics.items():
+            print(f" {metric_name}: {value:.5}")
+
 
 def test_algorithms(solved_board, n):
     first_algo_times = []
@@ -55,13 +65,33 @@ def test_algorithms(solved_board, n):
     return performance_metrics
 
 
+def time_function_and_log_to_file(func, file_name, num_runs, description):
+    total_elapsed_time = 0
+
+    for i in range(num_runs):
+        start_time = time.time()
+        func(i)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        total_elapsed_time += elapsed_time
+
+    average_elapsed_time = total_elapsed_time / num_runs
+    result = f"{func.__name__}\ndescription: {description}\n" \
+             f"average runtime (seconds) over {num_runs} runs: {average_elapsed_time:.6f}\n\n"
+    print(result)
+    with open(file_name, 'a') as file:
+        file.write(result)
+
+
+def run_csp(seed=None):
+    board = mask_board(NINE_X_NINE_SOLVED, seed=seed)
+    assignments = Assignments(board)
+    constraints = Constraints(assignments)
+    backtrack(constraints, assignments, mute=True)
+
+
 def main():
-    n = 50  # Number of times to run each algorithm
-    performance_metrics = test_algorithms(NINE_X_NINE_SOLVED, n)
-    for algo_name, metrics in performance_metrics.items():
-        print(f"{algo_name}:")
-        for metric_name, value in metrics.items():
-            print(f" {metric_name}: {value:.5}")
+    time_function_and_log_to_file(run_csp, "csp_test_results.txt", 100, "fixed bug in revise and fixed bug at start of infer, no prints")
 
 
 if __name__ == '__main__':
