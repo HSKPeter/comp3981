cf2bf0d72da341f2ece162e8e30495cbc0ae1601
Peter Hui <huishikuen@gmail.com>
2023-03-29 23:44:32 -0700

    Minor formatting

--- puzzle_loader.py
+++ puzzle_loader.py
@@ -1,18 +1,50 @@
-import json
+import os
 import random
 import copy
+
+
 class PuzzleLoader:
-    def load(self, size):
+
+    _package_directory = os.path.dirname(os.path.abspath(__file__))
+
+    @staticmethod
+    def read_file(filename):
+        with open(filename, 'r') as file:
+            return file.read()
+
+    def load_txt_file_in_standard_format(self, is_easy=False):
+        """
+        Loads a random sudoku puzzle of the correct size
+        """
+        sample = random.randint(1, 9)
+        difficulty = "easy" if is_easy else "hard"
+        file_path = os.path.join(self._package_directory, "assets", "standard_samples", "9x9", difficulty, f"{difficulty}_sample_{str(sample).zfill(2)}.txt")
+        file_content = self.read_file(file_path)
+        rows = file_content.split("\n")
+        result = []
+        for row in rows:
+            nums = [int(num) for num in row.split(",")]
+            result.append(nums)
+        return file_path, file_content, result
+
+    def load_from_2d_array_txt_file(self, size):
         """
         Loads a random sudoku puzzle of the correct size
         """
         sample = random.randint(0, 9)
-        with open('assets/solved_sudoku/sudoku{0}x{0}/{0}x{0}_sample_{1}.txt'.format(size, sample), 'r') as file:
-            contents = file.read()
-            board = json.loads(contents)
-            return board
-    
-    def mask_puzzle(self, board):
+        file_path = 'assets/solved_sudoku/sudoku{0}x{0}/{0}x{0}_sample_{1}.txt'.format(size, sample)
+        return self.read_file(file_path)
+
+    def load_unsolved_puzzle(self, size):
+        solved_board = self.load_from_2d_array_txt_file(size)
+        masked_board = self.mask_puzzle(solved_board)
+        return masked_board
+
+    def load_unsolved_9x9_puzzle_from_standard_samples(self, is_easy=False):
+        return self.load_txt_file_in_standard_format(is_easy)
+
+    @staticmethod
+    def mask_puzzle(board):
         """
         randomly sets 75% of a board to 0s and returns the board
         """
@@ -23,7 +55,7 @@ class PuzzleLoader:
         masked_values = 0
         col = 0
         row = 0
-        while (masked_values <= values_to_mask): 
+        while masked_values <= values_to_mask:
             if random.random() < 0.5:
                 board[row][col] = 0
                 masked_values += 1
@@ -35,10 +67,12 @@ class PuzzleLoader:
                     col = 0
                     row = 0
         return board
-                
+
+
 def main():
     loader = PuzzleLoader()
     print(loader.load(12))
+
 
 if __name__ == '__main__':
     main()
