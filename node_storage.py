0fafe53e6a0c47245292bf09581d5942c306d21c
Peter Hui <huishikuen@gmail.com>
2023-04-02 02:39:28 -0700

    Update

--- node_storage.py
+++ node_storage.py
@@ -13,22 +13,23 @@ class AzureStorageClient:
         return cls(cls._default_container_name)
 
     def __init__(self, azure_container_name=None, connection_string=None):
-        self.container_name = azure_container_name if azure_container_name is not None else uuid4().hex
-        AzureStorageClient._default_container_name = self.container_name
-
-        self.connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING") if connection_string is None else connection_string
-
-        if self.connection_string is None:
-            raise Exception("No connection string provided")
-
-        self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
-        self.container_client = None
-
-        if self.check_container_exists():
-            self.container_client = self.blob_service_client.get_container_client(self.container_name)
-        else:
-            self.create_container()
-            self.container_client = self.blob_service_client.get_container_client(self.container_name)
+        pass
+        # self.container_name = azure_container_name if azure_container_name is not None else uuid4().hex
+        # AzureStorageClient._default_container_name = self.container_name
+        #
+        # self.connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING") if connection_string is None else connection_string
+        #
+        # if self.connection_string is None:
+        #     raise Exception("No connection string provided")
+        #
+        # self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        # self.container_client = None
+        #
+        # if self.check_container_exists():
+        #     self.container_client = self.blob_service_client.get_container_client(self.container_name)
+        # else:
+        #     self.create_container()
+        #     self.container_client = self.blob_service_client.get_container_client(self.container_name)
 
     def check_container_exists(self):
         """
@@ -47,21 +48,21 @@ class AzureStorageClient:
         """
         Upload a json string to Azure Blob Storage as json file
         """
-        blob_client = self.container_client.get_blob_client(file_name)
-        blob_client.upload_blob(json_string, overwrite=True)
-        # with open(f"nodes/{file_name}", "w") as f:
-        #     f.write(json_string)
+        # blob_client = self.container_client.get_blob_client(file_name)
+        # blob_client.upload_blob(json_string, overwrite=True)
+        with open(f"nodes/{file_name}", "w") as f:
+            f.write(json_string)
 
     def download_data(self, file_name):
         """
         Download a json file from Azure Blob Storage
         """
-        blob_client = self.container_client.get_blob_client(file_name)
-        data = blob_client.download_blob().readall()
-        json_str = data.decode('utf-8')
-        return json.loads(json_str)
-        # with open(f"nodes/{file_name}", "r") as f:
-        #     return json.load(f)
+        # blob_client = self.container_client.get_blob_client(file_name)
+        # data = blob_client.download_blob().readall()
+        # json_str = data.decode('utf-8')
+        # return json.loads(json_str)
+        with open(f"nodes/{file_name}", "r") as f:
+            return json.load(f)
 
 
 if __name__ == "__main__":
