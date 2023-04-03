bef4dc6c88898c67335dcf478f2ba91599de681a
Peter Hui <huishikuen@gmail.com>
2023-04-03 14:38:40 -0700

    Fix bug in brute force

--- algo_util.py
+++ algo_util.py
@@ -7,11 +7,10 @@ FLOOR_SQUARE_ROOTS = {
 }
 
 
-def get_sub_square_index(n, row, col) -> int:
+def get_sub_square_index(n, row, col):
     sub_n = FLOOR_SQUARE_ROOTS[n]  # size of each sub-square
     sub_m = n // sub_n  # number of sub-squares in each row or column
-    sub_row = row // sub_n
-    sub_col = col // sub_m
+    sub_row = row // sub_m
+    sub_col = col // sub_n
     sub_square_index = sub_row * sub_m + sub_col
-    # sub_square_index starting from 0, counting from top-left to bottom-right of the board
     return sub_square_index
