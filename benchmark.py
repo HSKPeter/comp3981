9b906077afa58e9e83b33f4325bdf7ed1bc4744b
Peter Hui <huishikuen@gmail.com>
2023-04-04 12:32:06 -0700

    Update benchmark

--- benchmark.py
+++ benchmark.py
@@ -83,21 +83,30 @@ def main():
     benchmark_test_runner = BenchmarkTestRunner()
     puzzle_loader = PuzzleLoader()
 
-    file_path, raw_content, board = puzzle_loader.load_unsolved_puzzle(25,
-                                                                       sample_index=4,
-                                                                       is_easy=True)
-    report_content = f"Loaded puzzle from {file_path}:\n{raw_content}\n\n"
+    report_content = ""
 
-    print(report_content)
+    sizes = [9, 12, 16, 25]
+    for size in sizes:
+        for sample_index in range(1, 4):
 
-    algo_types_to_test = [
-        # Algorithm.BRUTE_FORCE,
-        AlgorithmType.CSP_RECURSIVE,
-        AlgorithmType.CSP_ITERATIVE,
-    ]
+            file_path, raw_content, board = puzzle_loader.load_unsolved_puzzle(size,
+                                                                               sample_index=sample_index,
+                                                                               is_easy=True)
 
-    for type_to_test in algo_types_to_test:
-        report_content += benchmark_test_runner.run_benchmark(board, type_to_test)
+            report_content = f"Loaded puzzle from {file_path}:\n{raw_content}\n\n"
+
+            print(report_content)
+
+            algo_types_to_test = [
+                AlgorithmType.BRUTE_FORCE,
+                AlgorithmType.CSP_RECURSIVE,
+                AlgorithmType.CSP_ITERATIVE,
+                AlgorithmType.CSP_ITERATIVE_MULTIPROCESS,
+            ]
+
+            for type_to_test in algo_types_to_test:
+                report_content += benchmark_test_runner.run_benchmark(board, type_to_test)
+
 
     benchmark_test_runner.write_report(report_content, uuid_in_filename=True)
 
